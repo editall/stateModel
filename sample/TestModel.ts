8b1964585fc166cc2018521acaabe2f14b6dd2bf
hika <hika@bsidesoft.com>
2022-12-03 21:52:12 +0900

    .

--- sample/TestModel.ts
+++ sample/TestModel.ts
@@ -3,6 +3,9 @@ import StateModel from "../src/StateModel";
 class TestModel extends StateModel{
     count = 0;
     inc = ()=>this.count++;
+    trigger(key:string, oldValue:any, newValue:any){
+        console.log(key, oldValue, newValue);
+    }
 }
 
 export default TestModel;
--- src/StateModel.ts
+++ src/StateModel.ts
@@ -1,29 +1,22 @@
 import { SetStateAction } from "react";
 
 export default class StateModel {
-    // @ts-ignore
-    #setTriggers:[{(oldValue:any, newValue:any):void}];
-    setTriggers(...triggers:[{(oldValue:any, newValue:any):void}]) {
-        this.#setTriggers = triggers;
-    }
+    trigger(key:string, oldValue:any, newValue:any){}
     init(s: {(value: SetStateAction<number>): void;}) {
         let count = 1;
         const state = ()=>s(count++);
-        const names = Reflect.ownKeys(this);
-        const len = names.length;
-        for(let i = 0; i < len; i++){
-            const name = names[i];
-            if(typeof name == "symbol" || name[0] == '_') continue;
+        Reflect.ownKeys(this).forEach(name=>{
+            if(typeof name == "symbol" || name[0] == '_') return;
             // @ts-ignore
             let value = this[name];
             Object.defineProperty(this, name, {
                 get: () => value,
                 set: (v)=>{
-                    if(this.#setTriggers) this.#setTriggers.forEach(f=>f(value, v));
+                    this.trigger(name, value, v);
                     value = v;
                     state();
                 }
-            })
-        };
+            });
+        });
     }
 }
